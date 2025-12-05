f0bd334dcb4299ede9cc2a57cfe0657358263e39
PurnimaYalla <purnimayalla4@gmail.com>
2025-12-05 08:50:13 +0530

    Add files via upload

--- matrix addition.cpp
+++ matrix addition.cpp
@@ -1,42 +1,44 @@
 #include<stdio.h>
 int main()
 {
-	int a[10][10],b[10][10],x[10][10],i,j,r,c,k;
-	printf("Enter row and column values\n");
+	int a[10][10],b[10][10],x[10][10],k,i,j,r,c;
+	printf("enter row and column values\n");
 	scanf("%d%d",&r,&c);
-	printf("Enter A matrix elements\n");
+	printf("enter A matrix elements\n");
 	for(i=0;i<r;i++)
 	{
-	for(j=0;j<c;j++)
-	{
-		scanf("%d",&a[i][j]);
+		for(j=0;j<c;j++)
+		{
+			scanf("%d\n",&a[i][j]);
+		}
 	}
-	}
-    printf("Enter B matrix elements\n");
+	printf("enter B matrix elements\n");
 	for(i=0;i<r;i++)
 	{
-	for(j=0;j<c;j++)
-	{
-		scanf("%d",&b[i][j]);
+		for(j=0;j<c;j++)
+		{
+			scanf("%d\n",&b[i][j]);
+		}
 	}
+	printf("matrix multiplication is:\n");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			x[i][j] = 0;
+			for(k=0;k<c;k++)
+			{
+				x[i][j] = x[i][j]+(a[i][k] * b[k][j]);
+			}
+		}
 	}
 	for(i=0;i<r;i++)
 	{
-	for(j=0;j<c;j++)
-	{
-	x[i][j]=a[i][j]+b[i][j];
-	}
-}
-    printf("Matrix Multiplication is:\n");
-	for(i=0;i<r;i++)
-	{
-	for(j=0;j<c;j++)
-	{
-		x[i][j]=0;
-		for(k=0;k<c;k++)
-	{
-	}
-	
+		for(j=0;j<c;j++)
+		{
+			printf("%d\t",x[i][j]);
+		}
+		printf("\n");
 	}
 	return 0;
 }
