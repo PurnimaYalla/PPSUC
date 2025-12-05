dfbc2d81f52c8791f201e6edccf7d95e001e6491
PurnimaYalla <purnimayalla4@gmail.com>
2025-12-05 08:43:19 +0530

    Add files via upload

---
+++ convert seconds to hours,minutes and seconds.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int total_seconds;
+    scanf("%d", &total_seconds);
+
+    int hours = total_seconds / 3600;
+    int remaining_seconds = total_seconds % 3600;
+    int minutes = remaining_seconds / 60;
+    int seconds = remaining_seconds % 60;
+
+    printf("H:M:S-%d:%d:%d", hours, minutes, seconds);
+
+    return 0;
+}
+
