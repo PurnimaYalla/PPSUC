0d97d9ee4f430b0f75a346fe9796bd709b88e58c
PurnimaYalla <purnimayalla4@gmail.com>
2025-12-05 08:36:14 +0530

    Add files via upload

---
+++ area and perimeter of circle.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int r;
+    scanf("%d", &r);
+
+    float pi = 3.14;
+    float area = pi * r * r;
+    float perimeter = 2 * pi * r;
+
+    printf("%.2f\n", area);
+    printf("%.2f\n", perimeter);
+
+    return 0;
+}
+
